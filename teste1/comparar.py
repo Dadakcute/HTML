a0f552dbec4a90fec710edfb22c3794cc3b8525a
Isack <isackcoelho731@gmail.com>
2025-09-02 11:30:03 -0300

    atualizações html

---
+++ teste1/comparar.py
@@ -0,0 +1,9 @@
+num1 = float(input("Digite um número:"))
+num2 = float(input("Digite um número:"))
+
+if num1 > num2:
+    print(f"{num1} é maior que {num2}")
+elif num1 == num2:
+    print(f"{num1} e {num2} são iguais!")
+else:
+    print(f"{num2} é maior que {num1}")    
